f088a0d1a0d4d3ade069cefa09544438d8f88292
Rogelio Martin Corsino <rcorsino@gmail.com>
2017-12-19 21:17:35 -0300

    Auto stash before merge of "TestBranch" and "origin/development"

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -5,13 +5,17 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { ArticuloComponent } from './components/articulo/articulo.component';
 import { IconButtonComponent } from './components/framework/icon-button/icon-button.component';
+import { HeaderComponent } from './components/header/header.component';
+import { ContentComponent } from './components/content/content.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ArticuloComponent,
     IconButtonComponent
+    ArticuloComponent,
+    HeaderComponent,
+    ContentComponent
   ],
   imports: [
     BrowserModule
